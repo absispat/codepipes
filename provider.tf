ec0790949418a4d4d61b5edd4ab7446e3fd2db24
absipatnoon <a.patil@noonacademy.com>
2021-05-26 18:57:50 +0530

    code pipes demo

--- provider.tf
+++ provider.tf
@@ -4,14 +4,14 @@ provider "aws" {
   # profile                 = "ccsandbox"
 }
 
-terraform {
-  backend "s3" {
-    encrypt                 = false
-    bucket                  = "absipatcodepipes"
-    dynamodb_table          = "absipatcodepipes"
-    region                  = "us-east-1"
-    key                     = "codepipes/terra.state"
-    # shared_credentials_file = "~/.aws/credentials"
-    # profile                 = "ccsandbox"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     encrypt                 = false
+#     bucket                  = "absipatcodepipes"
+#     dynamodb_table          = "absipatcodepipes"
+#     region                  = "us-east-1"
+#     key                     = "codepipes/terra.state"
+#     # shared_credentials_file = "~/.aws/credentials"
+#     # profile                 = "ccsandbox"
+#   }
+# }
